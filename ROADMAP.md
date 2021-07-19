37ba735d5c7452a6df14126204466dbb4d2283d7
Varoon <raghupathivaroon@gmail.com>
2021-07-19 13:29:55 -0400

    Implement counting words and characters

--- ROADMAP.md
+++ ROADMAP.md
@@ -2,7 +2,7 @@
 
 - [x] Set up chrome extension skeleton
 - [x] Find selected element and get text
-- [ ] Implement counting words and characters
+- [x] Implement counting words and characters
 - [ ] Show in left side modal (when clicking the dots to the left of a block)
 - [ ] Show in right click modal
 - [ ] Get selected text (via browser) when selecting text inside a block and render WC in small top side modal
--- src/content_script.ts
+++ src/content_script.ts
@@ -16,7 +16,46 @@ function main() {
 			if (x != currSelectedText) {
 				currSelectedText = x;
 				console.log(x);
+				console.log(countWordsAndChars(x));
 			}
 		}
 	}, 1000);
+}
+
+type WordsAndChars = {
+	words: number;
+	chars: number;
+};
+
+enum states {
+	OUT,
+	IN,
+}
+
+function countWordsAndChars(text: string): WordsAndChars {
+	text = text.trim();
+	let state = states.OUT;
+	let words = 0,
+		chars = text.length;
+
+	for (let c of text) {
+		if (state === states.IN) {
+			if (c == " ") {
+				state = states.OUT;
+				words++;
+			}
+		} else {
+			if (c != " ") {
+				state = states.IN;
+			}
+		}
+	}
+
+	// close final word
+	words++;
+
+	return {
+		words,
+		chars,
+	};
 }
