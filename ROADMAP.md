ed7c2347b5729c3ce1af88e8c6a766d592333bd2
Varoon <raghupathivaroon@gmail.com>
2022-03-16 17:01:59 -0400

    handle small top modal

--- ROADMAP.md
+++ ROADMAP.md
@@ -4,6 +4,7 @@
 - [x] Find selected element and get text
 - [x] Implement counting words and characters
 - [x] Show in left side modal (when clicking the dots to the left of a block)
+- [x] Show in small top modal (non-halo)
 - [x] Show in right click modal
   - [ ] Handle this separate modal: ![Image](images/small_rightclick_modal.png)
 - [ ] Handle end of line as a word break, ensure behavior with punctuation, newlines, tabs, etc.
--- src/content_script.ts
+++ src/content_script.ts
@@ -3,7 +3,6 @@ import $ from "jquery";
 window.addEventListener("load", main, false);
 
 let currSelectedText = "";
-
 function main() {
 	let words: number, chars: number;
 	//TODO: instead of every second, run on click?
@@ -15,9 +14,12 @@ function main() {
 			// only log on change
 			if (x != currSelectedText) {
 				currSelectedText = x;
-				({ words, chars } = countWordsAndChars(currSelectedText));
 			}
+		} else {
+			currSelectedText = window!.getSelection()!.toString();
 		}
+
+		({ words, chars } = countWordsAndChars(currSelectedText));
 
 		if (chars) {
 			// if chars === 0, then
@@ -27,14 +29,23 @@ function main() {
 			if (leftMenuItem.length && leftMenuItem.length <= 1) {
 				const leftMenuHTML = `<div style="font-size: 12px; line-height: 16px; color: rgba(255, 255, 255, 0.4); white-space: nowrap; overflow: hidden; text-overflow: ellipsis; margin-bottom: 4px;">${words} words, ${chars} chars</div>`;
 				leftMenuItem.before(leftMenuHTML);
-			} else {
-				const rightMenuItem = $(
-					"#notion-app > div > div.notion-overlay-container.notion-default-overlay-container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > div > div > div > div > div.notion-scroller.vertical > div:nth-child(2) > div > div > div > div"
-				);
-				if (rightMenuItem.length && rightMenuItem.length <= 1) {
-					const rightMenuHTML = `<div style="font-size: 12px; line-height: 16px; color: rgba(255, 255, 255, 0.4); margin-bottom: 4px;">${words} words, ${chars} chars</div>`;
-					rightMenuItem.before(rightMenuHTML);
-				}
+			}
+
+			const rightMenuItem = $(
+				"#notion-app > div > div.notion-overlay-container.notion-default-overlay-container > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > div > div > div > div > div.notion-scroller.vertical > div:nth-child(2) > div > div > div > div"
+			);
+			if (rightMenuItem.length && rightMenuItem.length <= 1) {
+				const rightMenuHTML = `<div style="font-size: 12px; line-height: 16px; color: rgba(255, 255, 255, 0.4); margin-bottom: 4px;">${words} words, ${chars} chars</div>`;
+				rightMenuItem.before(rightMenuHTML);
+			}
+
+			const topMenuItem = $(
+				'#notion-app > div > div.notion-overlay-container.notion-default-overlay-container > div:nth-child(2) > div.notion-text-action-menu > div:nth-child(2) > div'
+			);
+
+			if (topMenuItem.length && topMenuItem.children().length <= 7) {
+				const topMenuHTML = `<div class="notion-focusable" role="button" tabindex="0" style="user-select: none; transition: background 20ms ease-in 0s; cursor: pointer; box-shadow: rgba(255, 255, 255, 0.094) 0px 0px 0px 1px; padding: 2px 5px; align-self: center;">${words}w\xa0//\xa0${chars}c</div>`;
+				topMenuItem.append(topMenuHTML);
 			}
 		}
 	}, 200);
